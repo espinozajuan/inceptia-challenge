da9ebbd08e8ac3af9b2021864d6f996104416794
Juan Gabriel Espinoza <136479329+juanespinozaa@users.noreply.github.com>
2024-05-21 11:23:55 -0300

    add: login styling

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,48 +2,86 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { login } from '../services/api';
 
-const LoginContainer = styled.div`
+const LoginContainer = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  min-height: 100vh;
   background-color: #f0f0f0;
 `;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  width: 300px;
-  padding: 20px;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 16px;
+  width: 100%;
+  max-width: 400px;
+  padding: 20px 12px;
   border: 1px solid #ccc;
   border-radius: 5px;
   background-color: white;
+  box-sizing: border-box;
+`;
+
+const H1 = styled.h1`
+  text-align: center;
+  width: 100%;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 0px;
+`;
+
+const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+  width: 100%;
 `;
 
 const Input = styled.input`
-  margin: 10px 0;
   padding: 10px;
   width: 100%;
+  box-sizing: border-box;
   border: 1px solid #ccc;
-  border-radius: 5px;
+  border-radius: 4px;
+  transition: all 0.3s ease;
+
+  &:focus {
+    border-color: #66afe9;
+    outline: none;
+    box-shadow: 0 0 8px rgba(102, 175, 233, 0.6);
+  }
 `;
 
 const Button = styled.button`
   padding: 10px;
   border: none;
-  border-radius: 5px;
-  background-color: #007bff;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: 500;
+  width: 100%;
   color: white;
   cursor: pointer;
+  background-color: #50d8d7;
+  background-image: linear-gradient(316deg, #50d8d7 0%, #923993 74%);
+  transition: all 0.5s ease;
+
   &:hover {
-    background-color: #0056b3;
+    background-image: linear-gradient(90deg, #50d8d7 0%, #923993 74%);
   }
 `;
 
 const ErrorMessage = styled.div`
   color: red;
-  margin: 10px 0;
+  font-size: 14px;
+  font-weight: 500;
+  width: 100%;
+  text-align: center;
 `;
 
 interface LoginProps {
@@ -69,20 +107,24 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
   return (
     <LoginContainer>
       <Form onSubmit={handleLogin}>
-        <h1>Login</h1>
-        <Input
-          type='email'
-          placeholder='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error && <ErrorMessage>{error}</ErrorMessage>}
+        <H1>Login</H1>
+        <InputsContainer>
+          <Input
+            type='email'
+            placeholder='Email'
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type='password'
+            placeholder='Password'
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && <ErrorMessage>{error}</ErrorMessage>}
+        </InputsContainer>
         <Button type='submit'>Login</Button>
       </Form>
     </LoginContainer>
